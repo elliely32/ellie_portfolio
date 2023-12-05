155a9d60451ce24f8fda1fe0e4ccfaec8be61cd8
Ellie Ly <elliel.4797@gmail.com>
2023-12-04 23:14:35 -0500

    SETUP: React router dom setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import NavigationBar from './components/NavigationBar';
+import {Routes, Route} from 'react-router-dom';
 import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <>
+    <div >
       <NavigationBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/code" element={<LandingPage />} />
+        <Route path="/story" element={<LandingPage />} />
+        <Route path="/art" element={<LandingPage />} />
+        <Route path="/about" element={<LandingPage />} />
+      </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
