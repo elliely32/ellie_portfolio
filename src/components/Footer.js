471ef91a378474eee394f69dd36b392aa275f230
Ellie Ly <elliel.4797@gmail.com>
2023-12-04 23:20:15 -0500

    Fix: Links on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,26 +9,26 @@ const Footer = () =>{
         <div className="footerRoot">
             <p className="footerHook"> TELL ME YOUR STORY!</p>
             <div className="socialMediaIcons">
+                <a href="https://www.linkedin.com/in/elliely32/" target="_blank" rel="noreferrer">
                 <div className="socialMediaBubble">
-                    <a href="https://www.linkedin.com/in/elliely32/" target="_blank" rel="noreferrer">
-                        <img className="footerSNS" src={linkedin} alt={"linkedin logo"}/>
-                    </a>
+                    <img className="footerSNS" src={linkedin} alt={"linkedin logo"}/>
                 </div>
-                <div className="socialMediaBubble">
-                    <a href="https://www.instagram.com/sketchie32" target="_blank" rel="noreferrer">
+                </a>
+                <a href="https://www.instagram.com/sketchie32" target="_blank" rel="noreferrer">
+                    <div className="socialMediaBubble">
                         <img className="footerSNS" src={instagram} alt={"instagram logo"}/>
-                    </a>
-                </div>
-                <div className="socialMediaBubble">
-                    <a href="https://github.com/elliely32" target="_blank" rel="noreferrer">
+                    </div>
+                </a>
+                <a href="https://github.com/elliely32" target="_blank" rel="noreferrer">
+                    <div className="socialMediaBubble">
                         <img className="footerSNS" src={github} alt={"github logo"}/>
-                    </a>
-                </div>
-                <div className="socialMediaBubble">
-                    <a href="mailto:elliel.4797@gmail.com" target="_blank" rel="noreferrer">
+                    </div>
+                </a>
+                <a href="mailto:elliel.4797@gmail.com" target="_blank" rel="noreferrer">
+                    <div className="socialMediaBubble">
                         <img className="footerSNS" src={mail} alt={"mail logo"}/>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
             <p className="copyright">© 2023 Ellie Ly, All Rights Reserved.</p>
         </div>
